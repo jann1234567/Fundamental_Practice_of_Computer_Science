3511d29d3ea415d053fd0448cdd142c60b0d8174
honhaoyuan0 <promc1314@gmail.com>
2024-08-22 19:43:33 +0800

    Setup the boiler plate code

---
+++ ChatApplication/src/test/java/com/codepulse/sigmachat/SigmachatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codepulse.sigmachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigmachatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
