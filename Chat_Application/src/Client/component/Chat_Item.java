f6dd4b54ebf78f1376812da2f1bbb0d40ac2bcf0
Pang Jean Ann <pangjeanann@gmail.com>
2024-09-04 21:18:28 +0800

    add UI file

---
+++ Chat_Application/src/Client/component/Chat_Item.java
@@ -0,0 +1,111 @@
+
+package Client.component;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.border.EmptyBorder;
+
+
+public class Chat_Item extends javax.swing.JLayeredPane {
+   
+    private JLabel label;
+    
+    public Chat_Item() {
+        initComponents();
+        txt.setEditable(false);
+        txt.setBackground(new Color(0, 0, 0, 0));
+        txt.setOpaque(false);
+    }
+
+    public void setUserProfile(String user) {
+        JLayeredPane layer = new JLayeredPane();
+        layer.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        layer.setBorder(new EmptyBorder(10, 10, 0, 10));
+        JButton cmd = new JButton(user);
+        cmd.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        cmd.setBorder(null);
+        cmd.setContentAreaFilled(false);
+        cmd.setFocusable(false);
+        cmd.setForeground(new Color(30, 121, 213));
+        cmd.setFont(new java.awt.Font("sansserif", 1, 13));
+        txt.setBorder(javax.swing.BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        layer.add(cmd);
+        add(layer, 0);
+    }
+    
+    public void setText(String text) {
+        txt.setText(text);
+    }
+    @SuppressWarnings("unchecked")
+    
+    public void setTime(String time) {
+        JLayeredPane layer = new JLayeredPane();
+        layer.setLayout(new FlowLayout(FlowLayout.RIGHT, 0, 0));
+        layer.setBorder(new EmptyBorder(0, 5, 10, 5));
+        label = new JLabel(time);
+        label.setForeground(new Color(110, 110, 110));
+        label.setHorizontalTextPosition(JLabel.LEFT);
+        layer.add(label);
+        add(layer);
+    }
+    
+    public void setImage(boolean right, Icon... image) {
+        if (image.length > 0) {
+            JLayeredPane layer = new JLayeredPane();
+            layer.setLayout(new FlowLayout(right ? FlowLayout.RIGHT : FlowLayout.LEFT));
+            layer.setBorder(new EmptyBorder(0, 5, 0, 5));
+            Chat_Image chatImage = new Chat_Image(right);
+            chatImage.addImage(image);
+            layer.add(chatImage);
+            add(layer);
+        }
+    }
+    
+    public void sendSuccess() {
+        if (label != null) {
+            label.setIcon(new ImageIcon(getClass().getResource("/im/icon/tick.png")));
+        }
+    }
+    
+    public void seen() {
+        if (label != null) {
+            label.setIcon(new ImageIcon(getClass().getResource("/im/icon/double_tick.png")));
+        }
+    }
+    
+    public void hideText() {
+        txt.setVisible(false);
+    }
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        txt = new Client.swing.JIMSendTextPane();
+
+        setLayout(new javax.swing.BoxLayout(this, javax.swing.BoxLayout.PAGE_AXIS));
+
+        txt.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 5, 10));
+        txt.setSelectionColor(new java.awt.Color(89, 188, 255));
+        add(txt);
+    }// </editor-fold>//GEN-END:initComponents
+   
+     protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), 15, 15);
+        super.paintComponent(grphcs);
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private Client.swing.JIMSendTextPane txt;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ Chat_Application/src/Client/event/EventChat.java
@@ -0,0 +1,7 @@
+
+package Client.event;
+
+
+public interface EventChat {
+     public void sendMessage(String text);
+}
